ca253fc79c20613388be3aad7549fbf92e67860b
abirdhar87 <abirdhar87@gmail.com>
2025-03-31 12:13:14 +0530

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,10 @@
+val1=int(input("Enter the first number:- "))
+val2=int(input("Enter the second number:- "))
+add=val1+val2
+sub=val1-val2
+multi=val1*val2
+div=val1/val2
+print("Addition: ",add)
+print("Subtraction: ",sub)
+print("Multiplication: ",multi)
+print("Division: ",div)
---
+++ name_printing.py
@@ -0,0 +1,4 @@
+val1=input("Enter the first number:- ")
+val2=input("Enter the second number:- ")
+
+print(f"Hello, {val1+" "+val2}! Welcome to the python program")
